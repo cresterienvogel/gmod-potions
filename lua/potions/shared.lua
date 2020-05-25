f9e96355aa4ee971f3628f3ca51bc9b2039d08f6
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-05-25 23:50:31 +0700

    weapon switching during using is fixed now

--- lua/potions/shared.lua
+++ lua/potions/shared.lua
@@ -1,4 +1,14 @@
 POTIONS = POTIONS or {}
 
 POTIONS.Author = "crester"
-POTIONS.Build = "05/24/20"
+POTIONS.Build = "05/25/20"
+
+hook.Add("PlayerSwitchWeapon", "Potions", function(pl, old, new)
+    if not IsValid(old) or not string.find(old:GetClass(), "potion") then
+        return
+    end
+
+    if old:GetUsing() then
+        return true
+    end
+end)
